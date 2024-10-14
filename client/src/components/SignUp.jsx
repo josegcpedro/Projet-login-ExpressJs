c95fe8eab464fd90bd09e5f7ce2f05abdc9e9a3f
Jose <pedrojcunha7@hotmail.com>
2024-10-14 20:52:13 +0200

    Register user and add user to bd

--- client/src/components/SignUp.jsx
+++ client/src/components/SignUp.jsx
@@ -1,10 +1,30 @@
 import React from 'react';
+import { Link, Navigate } from 'react-router-dom';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 
 const SignUp = () => {
+    const [name, setName] = React.useState('');
+    const [email, setEmail] = React.useState('');
+    const [password, setPassword] = React.useState('');
+    const Navigate = useNavigate();
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        axios.post('http://localhost:3001/register', { name, email, password })
+            .then(result => {
+                console.log(result);
+                Navigate('/login'); 
+            })
+            .catch(err => {
+                console.log(err);
+            });
+    }
+
     return (
         <div className='vh-100 bg-light d-flex flex-column justify-content-center'>
             <div className="d-flex justify-content-center align-items-center">
-                <form className="w-50 p-4 bg-white shadow rounded">
+                <form className="w-50 p-4 bg-white shadow rounded" onSubmit={handleSubmit}>
                     <h1 className="text-center mb-4">Register</h1>
                     <div className="form-group mb-3">
                         <label htmlFor="name">Name</label>
@@ -14,6 +34,7 @@ const SignUp = () => {
                             id="name"
                             placeholder="Enter name"
                             name='name'
+                            onChange={(e) => setName(e.target.value)}
                         />
                     </div>
                     <div className="form-group mb-3">
@@ -24,6 +45,7 @@ const SignUp = () => {
                             id="exampleInputEmail1"
                             placeholder="Enter email"
                             name='email'
+                            onChange={(e) => setEmail(e.target.value)}
                         />
                     </div>
                     <div className="form-group mb-4">
@@ -34,6 +56,7 @@ const SignUp = () => {
                             id="exampleInputPassword1"
                             placeholder="Password"
                             name='password'
+                            onChange={(e) => setPassword(e.target.value)}
                         />
                     </div>
                     <div className="d-grid gap-1">
@@ -43,7 +66,7 @@ const SignUp = () => {
             </div>
             <div className="text-center mt-3">
                 <p>Already have an account?</p>
-                <button className="btn btn-secondary">Login</button>
+                <Link to="/login" className="btn btn-secondary">Login</Link>
             </div>
         </div>
     );
